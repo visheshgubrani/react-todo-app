aea9d76d928b2e33caeffebab3d818c5179d2a31
Vishesh <vishesh.gubrani@gmail.com>
2024-03-24 12:48:20 +0530

    added delete function

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,36 @@ import './App.css'
 function App() {
   const [todo, setTodo] = useState('')
   const [todoList, setTodoList] = useState([])
-  
+
+  const addTodo = () => {
+    setTodoList([...todoList, todo])
+    setTodo('')
+  }
+
+  const deleteTask = (index) => {
+    const newTasks = [...todoList]
+    newTasks.splice(index, 1)
+    setTodoList(newTasks)
+  }
+
+
   return (
     <div>
       <div className="addTodo">
         <input type="text" onChange={(e) => setTodo(e.target.value)}/>
-        <button>Add Task</button>
+        <button onClick={addTodo}>Add Task</button>
       </div>
 
-      <div className="Todos">{todo}</div>
+      <div className="Todos">
+        {todoList.map((task, index) => {
+          return (
+            <div>
+            <h2>{task}</h2>
+            <button onClick={() => deleteTask(index)}>Delete</button>
+            </div>
+          ) 
+        })}
+      </div>
     </div>
   )
 }
